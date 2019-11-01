32eebc5c0180566abbac846fea987a93189427cd
fabriciomsantos <fabriciomoreira19@gmail.com>
2019-11-01 09:53:27 -0300

    update singleton

--- Runtime/Singleton/Singleton.cs
+++ Runtime/Singleton/Singleton.cs
@@ -3,63 +3,59 @@
 namespace Tools.GamePatterns
 {
     [DisallowMultipleComponent]
-    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-
-        #region Fields
-
         /// <summary>
-        /// The instance.
+        /// The static reference to the instance.
         /// </summary>
-        private static T instance;
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Gets the instance.
-        /// </summary>
-        /// <value>The instance.</value>
         public static T Instance
         {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = FindObjectOfType<T>();
-                    if (instance == null)
-                    {
-                        GameObject obj = new GameObject();
-                        obj.name = typeof(T).Name;
-                        instance = obj.AddComponent<T>();
-                    }
-                }
-                return instance;
-            }
+            get;
+            protected set;
         }
 
-        #endregion
-
-        #region Methods
+        /// <summary>
+        /// Gets whether an instance of this singleton exists.
+        /// </summary>
+        public static bool InstanceExists => Instance != null;
 
         /// <summary>
-        /// Use this for initialization.
+        /// Gets the instance of this singleton, and returns true if it is not null.
+        /// Prefer this whenever you would otherwise use InstanceExists and Instance together.
+        /// </summary>
+        public static bool TryGetInstance(out T result)
+        {
+            result = Instance;
+
+            return result != null;
+        }
+
+        /// <summary>
+        /// Awake method to associate singleton with instance.
         /// </summary>
         protected virtual void Awake()
         {
-            if (instance == null)
+            if (Instance != null)
             {
-                instance = this as T;
-                DontDestroyOnLoad(gameObject);
+                Debug.LogWarningFormat("Trying to create a second instance of {0}", typeof(T));
+                Destroy(gameObject);
             }
             else
             {
-                Destroy(gameObject);
+                Instance = (T)this;
+                DontDestroyOnLoad(gameObject);
             }
         }
 
-        #endregion
-
+        /// <summary>
+        /// OnDestroy method to clear singleton association.
+        /// </summary>
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
     }
 }
